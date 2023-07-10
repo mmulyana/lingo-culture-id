987d3a21b1d1e3494e8e4154c52fa998076b3918
mulyant20 <mulyan.t20@gmail.com>
2023-07-11 06:28:38 +0700

    fix src and add Image

--- src/app/learn/[level]/page.js
+++ src/app/learn/[level]/page.js
@@ -1,6 +1,7 @@
 'use client'
 
 import { data } from '@/data'
+import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 import { useEffect, useState } from 'react'
 
@@ -32,34 +33,49 @@ export default function Page({ params }) {
     <div className='h-fit pb-20 grid grid-cols-[2fr_1fr] mt-8 gap-4 px-6'>
       <div className='h-fit'>
         <h1 className='text-xl font-semibold'>Baju adat di {params.level}</h1>
-        <div className='w-full bg-gray-100 h-80 rounded-xl mt-4'></div>
+        <div className='w-full bg-gray-100 h-fit rounded-xl mt-4 p-4'>
+          <Image
+            src={detail?.data[index].img}
+            alt='image'
+            width={500}
+            height={500}
+            className='w-full h-full object-cover'
+          />
+        </div>
       </div>
       <div className='mt-11'>
-        <p className='text-xl text-slate-800 font-semibold'>Baju {detail?.data[index].title}</p>
+        <p className='text-xl text-slate-800 font-semibold'>
+          Baju {detail?.data[index].title}
+        </p>
         <p className='font-semibold text-slate-700 mt-3'>Deskripsi</p>
         <p className='mt-2 text-slate-500'>{detail?.data[index].description}</p>
-        <button onClick={() => {
-          if(index === detail.data.length - 1) {
-            let local = localStorage.getItem('LEVEL')
-            if(local) {
-              let tmp = Number(local) + 1
-              console.log(tmp)
-              localStorage.setItem('LEVEL', tmp)
+        <button
+          onClick={() => {
+            if (index === detail.data.length - 1) {
+              let local = localStorage.getItem('LEVEL')
+              if (local) {
+                let tmp = Number(local) + 1
+                console.log(tmp)
+                localStorage.setItem('LEVEL', tmp)
+              } else {
+                localStorage.setItem('LEVEL', 1)
+              }
+              router.push('/')
             } else {
-              localStorage.setItem('LEVEL', 1)
+              let local = localStorage.getItem('POIN')
+              if (local) {
+                let tmp = Number(local) + 1
+                localStorage.setItem('POIN', tmp)
+              } else {
+                localStorage.setItem('POIN', 1)
+              }
+              setIndex((prev) => prev + 1)
             }
-            router.push('/')
-          } else {
-            let local = localStorage.getItem('POIN')
-            if(local) {
-              let tmp = Number(local) + 1
-              localStorage.setItem('POIN', tmp)
-            } else {
-              localStorage.setItem('POIN', 1)
-            }
-            setIndex(prev => prev + 1)
-          }
-        }} className='mt-4 rounded-xl px-6 py-2 bg-indigo-500 text-white shadow-[0_4px_0_0_rgb(67,56,202)]'>Selesai Baca</button>
+          }}
+          className='mt-4 rounded-xl px-6 py-2 bg-indigo-500 text-white shadow-[0_4px_0_0_rgb(67,56,202)]'
+        >
+          Selesai Baca
+        </button>
       </div>
     </div>
   )
--- src/data/index.js
+++ src/data/index.js
@@ -7,32 +7,32 @@ export const data = [
     data: [
       {
         title: 'batik sidoluhur',
-        img: '/public/img/batiksidoluhur.jpg',
+        img: '/img/batiksidoluhur.jpg',
         description:
           'Batik Sida Luhur (dibaca Sido Luhur; kadang juga digabung menjadi Sidaluhur atau Sidoluhur) adalah salah satu motif batik pedalaman yang cukup populer. Sesuai dengan namanya, motif ini bermakna harapan untuk mencapai kedudukan yang tinggi dan dapat menjadi panutan masyarakat (keluhuran).',
         is_read: false,
       },
       {
         title: 'batik tujuh rupa',
-        img: '/public/img/batiktujuhrupa.jpg',
+        img: '/img/batiktujuhrupa.jpg',
         description: 'Batik Tujuh Rupa merupakan batik yang berasal dari pekalongan jawa tengah. Batik ini telah mengalami akulturasi dengan budaya tiongkok pada abad 19.  Pada batik tujuh rupa menggambarkan suasana alam dengan tambahan flora dan fauna.Motif dari batik ini memiliki makna kefasihan dan kelembutan, batik ini memberikan gambaran kehidupan penduduk jawa pesisir yang mudah beradaptasi dengan kebudayaan luar. Untuk motif alam dan tambahan berbagai ornamen seperti flora dan fauna bermakna kehidupan yang damai untuk warga sekitar.',
         is_read: false,
       },
       {
         title: 'batik kawung',
-        img: '/public/img/batikkawung.jpg',
+        img: '/img/batikkawung.jpg',
         description: 'Batik kawung berasal dari Daerah Istimewa yogyakarta..  Batik kawung diyakini diciptakan pada masa salah satu sultan kerajaan mataram pada abad ke 13 dipulau jawa. Motif ini juga ditemukan pada ukiran dinding candi pada beberapa tempat di jawa. Batik ini digunakan hanya untuk keluarga keraton, namun saat ini ditinggalkan saat kerajaan mataram terbagi menjadi dua. Motif dari batik kawung berbentuk irisan buah kolang-kaling, motif ini bermakna segala tindakan dilakukan dilakukan untuk kebaikan dan tidak perlu mengumbarnya. Motif bunga teratai pada batik kawung mengisyaratkan kesucian dan umur yang panjang. Sisi Lain motif kawung bermakna supaya manusia bisa menjadi sosok manusia yang ideal dan unggul. Motif batik kawung dibagi menjadi 3 bagian yaitu , awung Picis, kawung Bribil, dan kawung Sen.',
         is_read: false,
       },
       {
         title: 'kencongan',
-        img: '/public/img/kencongan.jpg',
+        img: '/img/kencongan.jpg',
         description: 'Baju ini berasal dari D.I.Yogyakarta. Baju ini merupakan busana adat untuk anak laki-laki. Baju kencongan memiliki model miri dengan baju surjan namun dengan warna yang lebih terang. Baju ini diikuti dengan aksesoris seperti kain batik dengan, lonthong tritik, ikat pinggang berupa kamus songketan dengan cathok terbuat dari suwasa.',
         is_read: false,
       },
       {
         title: 'ageng',
-        img: '/public/img/ageng.jpg',
+        img: '/img/ageng.jpg',
         description: 'Baju Ageng  atau pakaian “Kepbrabon”  adalah pakaian adat Jawa yang memiliki keistimewaan dan keagungan yang tinggi. Busana Ageng biasanya dikenakan untuk upacara-upacara khusus atau upacara adat Jawa yang penting seperti pernikahan adat, rapat kerajaan, upacara keagamaan atau acara resmi lainnya. Pakaian Ageng mewakili status sosial yang tinggi dan menampilkan desain yang sangat spesifik dan dekorasi yang rumit. ',
         is_read: false,
       },
@@ -46,25 +46,25 @@ export const data = [
     data: [
       {
         title: 'king baba',
-        img: '/public/img/kingbaba.jpg',
+        img: '/img/kingbaba.jpg',
         description: 'Pakaian adat King Baba adalah pakaian adat yang digunakan oleh laki-laki dari suku Dayak. Pakaian ini terbuat dari kulit kayu kapuo yang telah dipukul-pukul hingga tersisa seratnya saja. Pakaian tersebut dijemur, dilukis dengan motif khas Suku Dayak, dan dihias dengan manik-manik berwarna jingga dan merah. King Baba merupakan pakaian yang sederhana namun memiliki nilai budaya dan keunikan tersendiri.',
         is_read: false,
       },
       {
         title: 'king bibinge',
-        img: '/public/img/kingbibinge.jpg',
+        img: '/img/kingbibinge.jpg',
         description: 'Pakaian adat Dayak terdiri dari King Baba untuk pria dan King Bibinge untuk wanita. Keduanya terbuat dari kerang dan dihiasi mutiara. King Baba lebih terbuka sedangkan King Bibinge lebih tertutup dengan pelindung dada, platform dan rok. Ada juga aksesoris seperti gelang, kalung dan bando dengan bulu buku. Kedua pakaian adat ini menampilkan keindahan dan keunikan melalui penggunaan bahan alam dan ornamen tradisional. Pakaian adat ini merupakan simbol identitas budaya suku Dayak yang kaya dan unik.',
         is_read: false,
       },
       {
         title: 'sangkarut',
-        img: '/public/img/sangkarut.jpg',
+        img: '/img/sangkarut.jpg',
         description: 'Baju Sangkarut atau Baju Basulau adalah pakaian adat suku Dayak Ngaju di Kalimantan Tengah. Baju ini berupa rompi yang dilapisi oleh kerang (sulau). Bahan yang digunakan terdiri dari serat daun nanas, daun lemba, tengang, dan nyamu. Kulit nyamu, yang merupakan kulit tumbuhan pinang puyuh, digunakan untuk dirajut menjadi rompi.Baju Sangkarut memiliki filosofi sebagai pembatas yang bisa melindungi pemakainya dari gangguan roh halus dan pengaruh orang jahat. Pada umumnya, baju ini digunakan dalam perang, dengan bahan-bahan azimat yang diyakini memberikan kekebalan terhadap senjata tajam atau senjata api. Lukisan, tempelan kulit trenggiling, kancing, uang logam, manik-manik, dan benda-benda azimat juga digunakan untuk menghias baju ini.',
         is_read: false,
       },
       {
         title: 'kingtompang',
-        img: '/public/img/kingtompang.jpg',
+        img: '/img/kingtompang.jpg',
         description: 'King Tompang merupakan pakaian adat yang memiliki sejarah penting dalam budaya masyarakat Kalimantan Barat. Busana ini memiliki warna dasar hitam, biru dan merah serta dihiasi dengan berbagai pola dan pita warna-warni yang membuatnya menarik. King Tompang terkenal di kalangan masyarakat Dayak dan sering digunakan. Meskipun desainnya sederhana, mereka sering dihiasi dengan dekorasi seperti benang dan pita warna-warni. Perpaduan ornamen tersebut membuat Raja Tompang terlihat unik, menarik dan cantik saat dikenakan.',
         is_read: false,
       },
@@ -78,25 +78,25 @@ export const data = [
     data: [
       {
         title: 'ulos karo',
-        img: '/public/img/uloskaro.jpg',
+        img: '/img/uloskaro.jpg',
         description: 'Pakaian adat suku Batak yang terkenal adalah kain adat khas Sumatera Utara. Ulos telah digunakan oleh masyarakat Batak sejak zaman dahulu dan memiliki filosofi yang mendalam terkait dengan kehidupan dan kehangatan tubuh. Laki-laki Batak memakai jas dengan bagian luar melilit badan, sedangkan perempuan memakai skizkebaya sebagai rok. Selain itu, juga digunakan sebagai selendang di bahu. Dalam Ulos terdapat aturan memberi dan menerima mengenai hubungan kekeluargaan, serta sifat dan nominalnya. Selain uis, ada juga uis tenun Kabupaten Karo yang proses pembuatannya mirip dengan uis namun dengan corak dan sejarah yang berbeda. Uis Karola memiliki warna dan motif yang berbeda, masing-masing dengan ceritanya sendiri. Pemanfaatan Karo Uis tidak terbatas pada upacara adat dan arak-arakan saja, tetapi sudah merambah ke dunia fashion dan aksesoris. ',
         is_read: false,
       },
       {
         title: 'ulos sibolang',
-        img: '/public/img/ulossibolang.png',
+        img: '/img/ulossibolang.png',
         description: 'Kain ulos digunakan sebagai simbol duka cita dalam budaya masyarakat Batak. Salah satu jenis ulos yang digunakan dalam situasi duka cita adalah Ulos Sibolang. Ulos Sibolang memiliki corak yang saling beriringan yang melambangkan kesuburan dan kekompakan. Ulos ini sering diberikan kepada seorang anak yang baru lahir, terutama anak pertama. Meskipun dulunya dipakai untuk keperluan duka dan sukacita, saat ini Ulos Sibolang dianggap sebagai simbol duka cita yang dipakai oleh keluarga dekat yang kehilangan anggota keluarga, seperti dalam kasus Ulos Saput untuk orang dewasa yang meninggal tetapi belum memiliki cucu, Ulos Tujung untuk janda dan duda, yang artinya mereka yang ditinggal mati oleh pasangan mereka. Penggunaan Ulos Sibolang dalam acara duka menunjukkan kedekatan hubungan dengan orang yang meninggal.',
         is_read: false,
       },
       {
         title: 'limpapeh rumah nan gadang',
-        img: '/public/img/limpapehrumahnangadang.jpg',
+        img: '/img/limpapehrumahnangadang.jpg',
         description: 'Pakaian adat Limpapeh Rumah Nan Gadang adalah pakaian tradisional yang digunakan di Sumatra Barat, terpengaruh oleh budaya Suku Minangkabau, Suku Mandailing, dan Suku Batak. Terdiri dari tengkuluk, baju batabue, minsie, sarung, dan salempang, pakaian ini memiliki motif dan corak yang terinspirasi dari kekayaan alam Sumatera. Aksesoris emas seperti kalung, gelang, dan cincin melengkapi pakaian adat ini. Limpapeh Rumah Nan Gadang melambangkan pentingnya peran ibu dalam masyarakat Minangkabau, yang bertanggung jawab terhadap pendidikan anak-anak dan keluarganya.',
         is_read: false,
       },
       {
         title: 'ulos ragi hotang',
-        img: '/public/img/ulosragihotang.png',
+        img: '/img/ulosragihotang.png',
         description: 'Ulos ragi hotang adalah jenis ulos yang bergaris atau bercorak rotan yang dikenakan pada upacara adat, khususnya pernikahan. Setiap tema luar ruang memiliki makna yang berhubungan dengan kehidupan. Motif hotang melambangkan ikatan antara pasangan untuk pernikahan yang harmonis. ',
         is_read: false,
       },
@@ -110,25 +110,25 @@ export const data = [
     data: [
       {
         title: 'labbu',
-        img: '/public/img/labbu.jpg',
+        img: '/img/labbu.jpg',
         description: 'Baju Labbu adalah pakaian adat khas Sulawesi Selatan yang awalnya digunakan oleh wanita bangsawan di kerajaan Luwu. Namun, saat ini baju Labbu dapat digunakan oleh semua wanita dalam berbagai acara adat atau pernikahan. Pakaian ini memiliki desain unik dengan lengan panjang dan ketat, serta menggunakan kain sutra tipis dan sarung lipa sebagai bawahan. Selain itu, baju Labbu juga memiliki motif seperti corak bunga pada bagian dada dan lengan.',
         is_read: false,
       },
       {
         title: 'bodo',
-        img: '/public/img/bodo.jpg',
+        img: '/img/bodo.jpg',
         description: 'Pakaian Bodo adalah pakaian adat suku Bugis Sulawesi Selatan. Desainnya unik dengan bentuk persegi dan lengan pendek. Warna pakaian Bodo memberikan informasi tentang usia dan status pemakainya. Terbuat dari kain muslim yang nyaman dipadukan dengan sarung kotak-kotak. ',
         is_read: false,
       },
       {
         title: 'pokko',
-        img: '/public/img/pokko.jpg',
+        img: '/img/pokko.jpg',
         description: 'Baju pokko merupakan pakaian adat suku Toraja Sulawesi Selatan yang umumnya dikenakan oleh kaum wanita. Baju ini memiliki lengan pendek dan warna-warna berani seperti kuning, merah dan putih. Asesoris seperti perhiasan dan manik-manik sering dipakai dan manik-manik diletakkan di dada. Pakaian pokko juga memiliki gelang dan ikat kepala atau ikat pinggang yang disebut kandure. Umumnya, pakaian Pokko hanya dikenakan pada acara-acara resmi. ',
         is_read: false,
       },
       {
         title: 'pattuqduq towanie',
-        img: '/public/img/pattuqduqtowanie.png',
+        img: '/img/pattuqduqtowanie.png',
         description: 'Baju Pattuqduq Towaine adalah pakaian adat suku Mandar di Sulawesi Selatan. Biasanya digunakan pada acara pernikahan atau pertunjukan tari Pattuqduq. Terdiri dari 18 bagian atau 24 bagian jika digunakan untuk pernikahan. Baju ini hadir dalam berbagai gaya termasuk sarung bermotif, sarung rantai, dan sarung pada pinggir bawah. Busana Pattuqduq Towaine dipadukan dengan berbagai ornamen baik di kepala maupun di tangan. Najuini lebih sering dikenakan oleh wanita, sedangkan pria mengenakan jaket berbalut sutra dengan celana panjang dan sarung di pinggang. Baju Pattuqduq Towaine melambangkan kecakapan dan kemahiran orang Mandarin dalam bekerja dan bertindak. ',
         is_read: false,
       },
@@ -142,7 +142,7 @@ export const data = [
     data: [
       {
         title: 'koteka',
-        img: '/public/img/koteka.png',
+        img: '/img/koteka.png',
         description: 'Koteka adalah pakaian tradisional yang digunakan oleh sebagian penduduk asli Pulau Papua untuk menutupi kemaluan laki-laki. Terbuat dari moncong burung taong-taong atau kulit labu. Ukuran dan bentuk koteka tidak berkaitan dengan status pemakainya, melainkan lebih terkait dengan aktivitas pengguna atau upacara adat. Setiap suku memiliki perbedaan bentuk koteka, seperti suku Yali yang lebih suka bentuk labu yang panjang. Namun, penggunaan koteka sehari-hari semakin berkurang dan sering hanya digunakan sebagai cenderamata. Di kawasan pegunungan seperti Wamena, koteka masih dipakai, sementara di kawasan pantai sulit ditemukan. Koteka merupakan penutup alat kelamin pria yang terbuat dari kulit labu air yang dikeringkan dan dikaitkan di pinggang. Penggunaan koteka juga memiliki kaitan dengan status sosial, di mana semakin tinggi kedudukan pria secara adat, ukuran koteka yang dikenakan juga semakin besar.',
         is_read: false,
       },
@@ -156,13 +156,13 @@ export const data = [
     data: [
       {
         title: 'batik bali',
-        img: '/public/img/batikbali.jpg',
+        img: '/img/batikbali.jpg',
         description: 'Batik Bali merupakan hasil penyebaran batik dari Pulau Jawa ke Bali. Masyarakat Bali memiliki keahlian seni yang tinggi, sehingga batik berkembang dengan pesat di sana. Motif batik Bali terinspirasi dari lingkungan alam dan budaya Bali, serta pengaruh dari daerah lain. Motifnya bervariasi, antara lain Sekar Jagad Bali, Teratai Banji, dan Poleng Biru. Batik Bali digunakan dalam berbagai keperluan, termasuk upacara adat, kehidupan sehari-hari, dan sebagai cinderamata wisatawan. Batik Bali memiliki ciri khas seperti aksen yang berbeda-beda, perpaduan motif tradisional dan modern, inspirasi dari kisah para Dewa, perpaduan budaya asli dan luar, penggunaan pewarna alami, dan penggunaan kain mori yang lebih berat.',
         is_read: false,
       },
       {
         title: 'baju Safari',
-        img: '/public/img/safari.jpeg',
+        img: '/img/safari.jpeg',
         description: 'Baju safari, yang umumnya memiliki nuansa putih atau pola tertentu, merupakan pakaian adat yang dikenakan dengan tambahan selempang dan hiasan kepala tradisional yang disebut udeng. Kamben pada baju safari dikemas dengan cara yang berbeda antara pria dan wanita. Pada pria, kamben dikenakan di pinggang dengan lipatan di bagian depan, sementara pada wanita, kamben dibungkus erat-erat untuk memastikan tidak ada lipatan atau renda terlihat. Selain itu, baju safari juga dilengkapi dengan saku yang ditempatkan di sisi kiri atau kanan. Dengan ciri-ciri uniknya, baju safari menciptakan kesan yang khas dan memikat bagi mereka yang mengenakannya.',
         is_read: false,
       },
